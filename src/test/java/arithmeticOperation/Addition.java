9ef163b8b5379addabb3c9afe7e6bd3bd32cb75a
abhinav <777abhi@gmail.com>
2019-11-13 07:06:44 +0000

    FP - TestNG - Group Tags

--- src/test/java/arithmeticOperation/Addition.java
+++ src/test/java/arithmeticOperation/Addition.java
@@ -1,17 +1,19 @@
 package test.java.arithmeticOperation;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Addition {
 
-    @Test(enabled = false)
+    @Test(groups = { "checkintest" })
     public void sampleTest(){
 
         System.out.println("Test - Addition Passed");
+        Assert.assertEquals(1, 1);
 
     }
 
-    @Test
+    @Test(groups = { "checkintest" })
     public void sampleTest1(){
 
         System.out.println("Test - Addition Passed -sampleTest1");
