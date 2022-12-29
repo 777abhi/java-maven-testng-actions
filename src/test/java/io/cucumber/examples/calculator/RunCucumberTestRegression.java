3ef4766b4209d3aae1a70495c7d14ee4c8d3cc92
AB <44663451+777abhi@users.noreply.github.com>
2022-12-29 11:53:18 +0000

    fix : Test class name

--- src/test/java/io/cucumber/examples/calculator/RunCucumberTestRegression.java
+++ src/test/java/io/cucumber/examples/calculator/RunCucumberTestRegression.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
 @CucumberOptions(plugin = { "html:target/results.html", "message:target/results.ndjson" }, tags = "@regression")
-public class RunCucumberTestSanity extends AbstractTestNGCucumberTests {
+public class RunCucumberTestRegression extends AbstractTestNGCucumberTests {
 
     @DataProvider(parallel = true)
     @Override
